2ad4239070259550a27daacef5dde347755519df
Mahesh Gunda <maheshgunda405@gmail.com>
2026-02-24 15:40:36 +0530

    Fixed port binding issue

--- app.py
+++ app.py
@@ -268,4 +268,6 @@ def logout():
     session.pop("user", None)
     return redirect("/")
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
